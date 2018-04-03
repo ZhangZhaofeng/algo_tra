96c2e97556c87dc9f33a3923bd8119e1d3ad692c
ZhangZhaofeng <Phoenixflame11@gmail.com>
2018-04-03 18:42:36 +0900

    Add files via upload

---
+++ sigma_trend.py
@@ -0,0 +1,24 @@
+
+import technical_test
+import talib
+from talib import MA_Type
+from talib.abstract import *
+
+class Sigmatrend(technical_test.GMMA):
+
+    def get_bollinger(self, num=1000, periods="1H"):
+        (time_stamp, open_price, high_price, low_price, close_price) = self.btc_charts.get_price_array_till_finaltime(
+            num=num, periods=periods, converter=False)
+        (upper, middle, lower) = talib.BBANDS(close_price.T[0], timeperiod=20, nbdevup=0.5, nbdevdn=0.5 ,matype=MA_Type.EMA)
+
+        ma = talib.EMA(close_price.T[0], 60)
+        #(upper, middle, lower) = BBANDS(ma[-40:], 20, 0.5, 0.5)
+        return(upper[-10:],middle[-10:],lower[-10:])
+        #return([upper[-40:],middle[-40:],lower[-40:],close_price[-40:],ma[-40:]])
+
+
+
+if __name__ == '__main__':
+    ST = Sigmatrend()
+    result = ST.get_bollinger()
+    print(result)
