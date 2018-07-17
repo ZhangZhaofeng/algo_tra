198ffd67b40305ad1b262118891a0f64e94f4689
ZhangZhaofeng <Phoenixflame11@gmail.com>
2018-07-17 16:03:55 +0900

    Add files via upload

--- auto_bf_var_amount.py
+++ auto_bf_var_amount.py
@@ -126,29 +126,25 @@ class AutoTrading:
 
     def trade_market(self, type, amount):
         self.maintance_time()
-        self.maintance_time()
+
         product = 'FX_BTC_JPY'
         print('trade bitflyer')
         expire_time = 575
         try_t = 0
         while try_t < 20:
             if type == 'BUY' or type == 'buy':
-                parameters = [{'product_code': product, 'condition_type': 'MARKET', 'side': 'BUY',
-                            'size': str(amount)}]
-                order = self.bitflyer_api.sendparentorder(order_method='SIMPLE', minute_to_expire=expire_time,
-                                                      parameters=parameters)
+                order = self.bitflyer_api.sendchildorder(product_code=product, child_order_type='MARKET',
+                    side='BUY', size= str(amount))
                 data2csv.data2csv(
                     [time.strftime('%b:%d:%H:%M'), 'order', 'BUY_MARKET', 'amount', '%f' % float(amount)])
             elif type == "SELL" or type == "sell":
-                parameters = [{'product_code': product, 'condition_type': 'MARKET', 'side': 'SELL',
-                           'size': str(amount)}]
-                order = self.bitflyer_api.sendparentorder(order_method='SIMPLE', minute_to_expire=expire_time,
-                                                      parameters=parameters)
+                order = self.bitflyer_api.sendchildorder(product_code=product, child_order_type='MARKET',
+                                                         side='SELL', size=str(amount))
                 data2csv.data2csv(
                     [time.strftime('%b:%d:%H:%M'), 'order', 'SELL_MARKET', 'amount', '%f' % float(amount)])
             else:
                 print("error!")
-            if 'parent_order_acceptance_id' in order:
+            if 'child_order_acceptance_id' in order:
                 return (order)
             else:
                 try_t += 1
@@ -211,7 +207,131 @@ class AutoTrading:
         tdelta = b - a
         return(tdelta)
 
-    # get current price
+    def judge_order(self, id):
+        i = 20
+        while i > 0:
+            try:
+                order = self.get_orderbyid(id)
+                if order['parent_order_state'] == 'REJECTED':
+                    predict.print_and_write('Order rejected')
+                    return True
+                else:
+                    return False
+            except Exception:
+                time.sleep(5)
+                print(Exception)
+                print('Exception Try again')
+                i -= 1
+        predict.print_and_write('Try many times but no result, return False without confidence')
+        return False
+
+    def get_hilo(self):
+        i = 0
+        while i < 30:
+            try:
+                hilos = technical_fx_bidirc.HILO()
+                result = hilos.publish_current_hilo_price()
+
+        # result = prediction.publish_current_limit_price(periods="1H")
+                sell = float(result[1])
+                buy = float(result[0])
+                close = float(result[2])  # the close price of last hour
+                return([sell, buy, close])
+            except Exception:
+                print(Exception)
+                predict.print_and_write('Try to get hilo again')
+                time.sleep(10)
+                i+=1
+                continue
+
+    def get_orders(self, status = ''):
+        #order = self.quoinex_api.get_orders()
+        #order = self.quoinex_api.get_orders(status, limit)
+        #ACTIVE CANCELED
+        product = 'FX_BTC_JPY'
+        if status != '':
+            order = self.bitflyer_api.getparentorders(product_code=product, parent_order_state=status)
+        else:
+            order = self.bitflyer_api.getparentorders(product_code=product, count=30)
+        return (order)
+
+    def get_orderbyid(self, id):
+        product = 'FX_BTC_JPY'
+        i = 20
+        while i > 0:
+            try:
+            #order = self.bitflyer_api.getparentorder(product_code=product, parent_order_acceptance_id=id)
+                orders = self.get_orders()
+                for i in orders:
+                    if i['parent_order_acceptance_id'] == id:
+                        return (i)
+                print('order not find')
+                return({})
+            except Exception:
+                print('Server is fucked off ,try again')
+                time.sleep(20)
+                i -= 1
+                continue
+        print('Try too many times, failed')
+        return({})
+
+    def cancel_order(self, id):
+        product = 'FX_BTC_JPY'
+        i = 20
+        while i > 0:
+            try:
+                statue = self.bitflyer_api.cancelparentorder(product_code=product, parent_order_acceptance_id=id)
+                time.sleep(10)
+                order = self.get_orderbyid(id)
+                if order['parent_order_state'] == 'COMPLETED':
+                    predict.print_and_write('Order completed')
+                    return (0.0)
+                if order['parent_order_state'] == 'CANCELED' or order['parent_order_state'] == 'REJECTED':
+                    predict.print_and_write('Order cancelled')
+                    return (float(order['cancel_size']))
+                else:
+                    i -= 1
+                    print('Try again cancelling')
+                    continue
+            except Exception:
+                order = self.get_orderbyid(id)
+                if order['parent_order_state'] == 'COMPLETED':
+                    print('Executed before cancelling')
+                    return(0.0)
+                time.sleep(5)
+                print('Exception Try again cancelling')
+                i -= 1
+        predict.print_and_write('Cancel failed,( May be just lag)')
+        return (0.0)
+
+    def get_checkin_price(self):
+        p = self.bitflyer_api.getpositions(product_code='FX_BTC_JPY')
+        position0 = 0.0
+        checkin_price = 0.0
+        #time_diff = 0
+        if isinstance(p, list):
+            for i in p:
+                #predict.print_and_write('check in price: %f' % (i['price']))
+
+                if i['side'] == 'SELL':
+                    position0 -= i['size']
+                else:
+                    position0 += i['size']
+
+            for i in p:
+                checkin_price += i['size']/abs(position0) * i['price']
+            predict.print_and_write('Check in price: %f, position: %f' % (checkin_price, position0))
+
+            # for i in p:
+            #     opentime = i['open_date']
+            #     time_diff = self.bf_timejudge(opentime)
+            #     break
+
+        elif isinstance(p, dict) or len(p) == 0:
+            predict.print_and_write('Position not exist')
+        checkin_price = math.floor(checkin_price)
+        return([checkin_price, position0])
+
     def get_current_price(self, numbers):
         trade_history = self.bitflyer_api.executions(product_code = 'FX_BTC_JPY', count = 100)
         total_size = 0.0
@@ -225,61 +345,24 @@ class AutoTrading:
         return(math.floor(cur_price))
 
 
-    def get_hilo(self):
-        hilos = technical_fx_bidirc.HILO()
-        result = hilos.publish_current_hilo_price()
-
-        # result = prediction.publish_current_limit_price(periods="1H")
-        buy = float(result[0])
-        sell = float(result[1])
-        close = float(result[2])  # the close price of last hour
-        return ([sell, buy, close])
-
-
-    def get_checkin_price(self):
-        p = self.bitflyer_api.getpositions(product_code='FX_BTC_JPY')
-        position0 = 0.0
-        checkin_price = 0.0
-        time_diff = 0
-        if isinstance(p, list):
-            for i in p:
-                #predict.print_and_write('check in price: %f' % (i['price']))
-                if i['side'] == 'SELL':
-                    position0 -= i['size']
-                else:
-                    position0 += i['size']
-
-            for i in p:
-                checkin_price += i['size']/abs(position0) * i['price']
-            predict.print_and_write('Check in price: %f, position: %f' % (checkin_price, position0))
-
-            for i in p:
-                opentime = i['open_date']
-                time_diff = self.bf_timejudge(opentime)
-                break
-
-        elif isinstance(p, dict) or len(p) == 0:
-            predict.print_and_write('Position not exist')
-        checkin_price = math.floor(checkin_price)
-        return([checkin_price, position0, time_diff])
-
-
 
     def trade_hour_init(self, checkins, cur_price, hilo): # if we have a new position in this hour
         self.switch_in_hour = False
         if hilo[2] >= hilo[1] : # if close > hi and position is negative
             #TODO
-            trade_mount = '%.2f' % (self.init_trade_amount - checkins[1])
+            trade_mount = self.init_trade_amount - checkins[1]
             if trade_mount > 0.0:
-                order = self.trade_market('buy', trade_mount)
+                amount_str = '%.2f' % (trade_mount)
+                order = self.trade_market('buy', amount_str)
             # buy self.init + checkins[1]
 
         elif hilo[2] <= hilo[0] : # if close < lo and position is positive
             # TODO
             # sell self.init + checkins[1]
-            trade_mount = '%.2f' % (self.init_trade_amount + checkins[1])
+            trade_mount = self.init_trade_amount + checkins[1]
             if trade_mount > 0.0:
-                order = self.trade_market('sell', trade_mount)
+                amount_str = '%.2f' % (trade_mount)
+                order = self.trade_market('sell', amount_str)
 
         elif cur_price > hilo[0] and cur_price < hilo[1]:
             # TODO
@@ -296,8 +379,7 @@ class AutoTrading:
                 time.sleep(3)
             # set an oco
             order = self.trade_oco1(hilo[0], hilo[1], self.init_trade_amount)
-            self.order_exist == True
-        return(order)
+            self.order_exist = True
 
         # check the position
 
@@ -308,12 +390,12 @@ class AutoTrading:
     # following profit ever 1 min, if profit is starting to reduce , quite (need a trial order)
     def trade_in_hour(self, initposition, starttime):
         trial_loss_cut = 1500 # if the dynamic loss touches 2000, quit position.
-        time.sleep(60)
+        time.sleep(1)
         self.switch_in_hour = False
         tdelta = self.bf_timejudge(starttime)
         while tdelta < 3600:
             checkins = self.get_checkin_price()
-            new_position = float('%.2f' % (math.floor(checkins[1])))
+            new_position = float('%.2f' % (checkins[1]))
             if initposition != 0 and new_position * initposition < 0:
                 self.switch_in_hour = True
             elif initposition == 0.0 and new_position != 0.0:
@@ -321,8 +403,9 @@ class AutoTrading:
             if self.switch_in_hour:
                 predict.print_and_write('switch in hour')
                 self.trial_order(checkins, trial_loss_cut, starttime)
+                self.switch_in_hour = False
             else:
-                predict.print_and_write('no switch happened in hour')
+
                 time.sleep(60)
             tdelta = self.bf_timejudge(starttime)
 
@@ -342,11 +425,11 @@ class AutoTrading:
             if profit < pre_profit:
                 # TODO quit
                 if checkins[1] > 0.0:
-                    trade_mount = abs('%.2f' % (checkins[1]))
+                    trade_mount = '%.2f' % abs(checkins[1])
                     order = self.trade_market('sell', trade_mount)
                     predict.print_and_write(order)
                 elif checkins[1] < 0.0:
-                    trade_mount = abs('%.2f' % (checkins[1]))
+                    trade_mount = '%.2f' % abs(checkins[1])
                     order = self.trade_market('buy', trade_mount)
                     predict.print_and_write(order)
                 predict.print_and_write('quit position')
@@ -357,7 +440,9 @@ class AutoTrading:
                 return
 
             elif profit >= pre_profit and profit > 0:
-                pre_profit = profit - trial_loss_cut
+                temp_pre_profit = profit - trial_loss_cut
+                if temp_pre_profit > pre_profit:
+                    pre_profit = temp_pre_profit
             time.sleep(60)
             tdelta = self.bf_timejudge(starttime)
 
@@ -366,16 +451,18 @@ class AutoTrading:
         if checkins[1] < 0.0:
             # TODO
             # buy double
-            trade_mount = '%.2f' % (self.init_trade_amount - checkins[1])
+            trade_mount = self.init_trade_amount - checkins[1]
             if trade_mount > 0.0:
-                order = self.trade_stop('buy', hilo[1],  trade_mount)
+                amount_str = '%.2f' % (trade_mount)
+                order = self.trade_stop('buy', hilo[1],  amount_str)
                 self.order_exist == True
         elif checkins[1] > 0.0:
             # TODO
             # sell double
-            trade_mount = '%.2f' % (self.init_trade_amount + checkins[1])
+            trade_mount = self.init_trade_amount + checkins[1]
             if trade_mount > 0.0:
-                order = self.trade_stop('sell', hilo[0], trade_mount)
+                amount_str = '%.2f' % (trade_mount)
+                order = self.trade_stop('sell', hilo[0], amount_str)
                 self.order_exist == True
         return order
 
@@ -397,17 +484,42 @@ class AutoTrading:
                 predict.print_and_write('No position exist, trade none position')
             else:
                 predict.print_and_write('Trade with position and init position')
-            self.order_id = self.trade_hour_init(checkins, cur_price, hilo)
+            self.trade_hour_init(checkins, cur_price, hilo)
+            #self.order_id = order['parent_order_acceptance_id']
+
             # we should verify the order is dealing or not here
-            time.sleep(60) # in 200s , we should obvious the position change.
+            time.sleep(1) # in 200s , we should obvious the position change.
             checkins = self.get_checkin_price()
-            self.order_id = self.update_order(checkins, hilo)
+            order = self.update_order(checkins, hilo)
+            self.order_id = order['parent_order_acceptance_id']
 
         elif checkins[1] != 0.0 and not self.switch_in_hour:
-            self.order_id = self.update_order(checkins, hilo)
+            order = self.update_order(checkins, hilo)
+            self.order_id = order['parent_order_acceptance_id']
 
         self.trade_in_hour(checkins[1], starttime)
 
+    def get_collateral(self):
+        try:
+            result = self.bitflyer_api.getcollateral(product_code = 'FX_BTC_JPY')
+            data2csv.data2csv(result)
+            predict.print_and_write(result)
+        except Exception:
+            predict.print_and_write(Exception)
+
+def sendamail(title ,str):
+    address = 'phoenixflame11@rakuten.jp'  # change the reciver e-mail address to yours
+    username = 'goozzfgle@gmail.com'
+    paswd = ''
+
+    mail_str = '%s %s' % (str, formatdate(None, True, None))
+    sender = SendMail(address, username, paswd)
+    msg = MIMEText(mail_str)
+    msg['Subject'] = title
+    msg['From'] = username
+    msg['To'] = address
+    msg['Date'] = formatdate()
+    sender.send_email(address, msg.as_string())
 
 if __name__ == '__main__':
     argvs = sys.argv
@@ -417,5 +529,16 @@ if __name__ == '__main__':
         autoTrading.switch_in_hour = bool(sys.argv[1])
 
     #tdelta = autoTrading.bf_timejudge('2018-05-21T14:35:44.713')
-    while 1:
-        autoTrading.judge_condition()
+    try_times = 20
+    #tdelta = autoTrading.bf_timejudge('2018-05-21T14:35:44.713')
+    while try_times > 0:
+        try:
+            while 1:
+                autoTrading.judge_condition()
+                autoTrading.get_collateral()
+        except Exception:
+            print(Exception)
+            sendamail('Exception', 'exception happend')
+            predict.print_and_write('Exception happened, try again')
+            predict.print_and_write('Last try times: %d'%try_times)
+            try_times -= 1
