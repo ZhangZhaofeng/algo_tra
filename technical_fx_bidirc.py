397c7c0cfc3cb0085eae6dd0443d07963e2b6e26
ZhangZhaofeng <Phoenixflame11@gmail.com>
2018-07-17 16:13:37 +0900

    Update technical_fx_bidirc.py

--- technical_fx_bidirc.py
+++ technical_fx_bidirc.py
@@ -24,7 +24,7 @@ class HILO:
         return x.T
 
     def get_HIGH_MA(self, HIGH):  # price=1*N (N>61)
-        ma_high=self.MA(HIGH,20)
+        ma_high=self.MA(HIGH,23)
         return ma_high
 
     def get_LOW_MA(self, LOW):  # price=1*N (N>61)
