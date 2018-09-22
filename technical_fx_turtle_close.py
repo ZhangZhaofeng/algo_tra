5996716be5b189590a874f7d775e1f861e449324
ZhangZhaofeng <Phoenixflame11@gmail.com>
2018-09-23 00:13:08 +0900

    Add files via upload

--- technical_fx_turtle_close.py
+++ technical_fx_turtle_close.py
@@ -47,15 +47,37 @@ class HILO:
     def publish_current_hilo_price(self, num=100, periods="1H"):
         (time_stamp, open_price, high_price, low_price, close_price) = self.btc_charts.get_price_array_till_finaltime(
             final_unixtime_stamp=time.time(), num=num, periods=periods, converter=True)
-
-        low_price_ma = self.get_short_price(close_price)
-        high_price_ma = self.get_long_price(close_price)
-        low_price_ma_half = self.get_short_price(close_price, 30)
-        high_price_ma_half = self.get_long_price(close_price, 30)
+        midhigh, midlow = self.get_mid_hilo(open_price, high_price, low_price, close_price)
+        low_price_ma = self.get_short_price(midlow)
+        high_price_ma = self.get_long_price(midhigh)
+        low_price_ma_half = self.get_short_price(midlow, 30)
+        high_price_ma_half = self.get_long_price(midhigh, 30)
         (buyprice, sellprice)=(high_price_ma[-2][0],low_price_ma[-2][0])
         (quitshort, quitlong) = (high_price_ma_half[-2][0], low_price_ma_half[-2][0])
         #print(a)
         return (int(buyprice), int(sellprice), int(close_price[-1]), int(high_price[-1]), int(low_price[-1]), int(quitshort), int(quitlong))
+
+    def get_mid_hilo(self, open, high, low, close):
+        mid_factor = 0.5
+        mid_high = []
+        mid_low = []
+        for i in range(0,len(open)):
+            if open[i] != 0 and high[i] != 0 and low[i] != 0 and close[i] != 0:
+                if open[i] > close[i]:
+                    midhigh = (high[i] - open[i]) * mid_factor + open[i]
+                    midlow = close[i] - (close[i] - low[i]) * mid_factor
+                else:
+                    midhigh = (high[i] - close[i]) * mid_factor + close[i]
+                    midlow = open[i] - (open[i] - low[i]) * mid_factor
+                mid_high.append(midhigh)
+                mid_low.append(midlow)
+            else:
+                mid_high.append(0)
+                mid_low.append(9999999)
+        nparray_mid_high = np.array(mid_high)
+        nparray_mid_low = np.array(mid_low)
+        return(nparray_mid_high, nparray_mid_low)
+
 
 if __name__ == '__main__':
     hilo = HILO()
